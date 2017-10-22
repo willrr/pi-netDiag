d604a8673b3ba6f555e362cfea77efd2f30ccdde
willrr <will@willrr.co.uk>
2017-10-22 17:06:05 +0100

    Installer fixes

--- install.sh
+++ install.sh
@@ -3,17 +3,13 @@
 
 cd /home/pi
 sudo apt-get -y install python3-tk fping vim unzip
-wget https://github.com/willrr/pi-networkDiagnostics/archive/master.zip
-unzip ./master.zip
-cp -r ./pi-networkDiagnostics-master/ ./pi-networkDiagnostics/
-rm -r ./pi-networkDiagnostics-master/
 cd ./pi-networkDiagnostics/
 cat > launcher.sh
-echo "#!/#!/bin/sh" >> launcher.sh
+echo "#!/bin/sh" >> launcher.sh
 echo "# launcher.sh" >> launcher.sh
 echo "cd /" >> launcher.sh
 echo "cd home/pi/pi-networkDiagnostics" >> launcher.sh
 echo "python3 willrr.py" >> launcher.sh
 echo "cd /" >> launcher.sh
-sudo sed '3i@/home/pi/pi-networkDiagnostics/launcher.sh' /home/pi/.config/lxsession/LXDE-pi/autostart
+sed '3i@/home/pi/pi-networkDiagnostics/launcher.sh' /home/pi/.config/lxsession/LXDE-pi/autostart
 echo "Installed"
